ce7784ee7c582b971dbd4ea9f0f157e1c1644c8e
Abdullahishtiaq488 <abdullahishtiaq488@gmail.com>
2025-02-14 01:27:22 +0500

    improvement

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -1,16 +1,21 @@
 import ProductCard from '@/components/ProductCard'
 import { getSearchedProducts } from '@/lib/actions/actions'
 
-const SearchPage = async ({ params }: { params: { query: string }}) => {
-  const searchedProducts = await getSearchedProducts(params.query)
+interface SearchPageProps {
+  params: Promise<{ query: string }>;
+}
 
-  const decodedQuery = decodeURIComponent(params.query)
+const SearchPage = async ({ params }: SearchPageProps) => {
+  const { query } = await params;
+  const searchedProducts = await getSearchedProducts(query);
+
+  const decodedQuery = decodeURIComponent(query);
 
   return (
     <div className='px-10 py-5'>
       <p className='text-heading3-bold my-10'>Search results for {decodedQuery}</p>
-      {!searchedProducts || searchedProducts.length === 0 && (
-        <p className='text-body-bold my-5'>No result found</p>
+      {(!searchedProducts || searchedProducts.length === 0) && (
+        <p className='text-body-bold my-5'>No results found</p>
       )}
       <div className='flex flex-wrap justify-between gap-16'>
         {searchedProducts?.map((product: ProductType) => (
@@ -23,4 +28,4 @@ const SearchPage = async ({ params }: { params: { query: string }}) => {
 
 export const dynamic = "force-dynamic";
 
-export default SearchPage
+export default SearchPage;
